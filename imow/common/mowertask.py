21103ce9644bb4ef0e35c765671e21dc91463dd9
chris <chris@homeset.de>
2021-06-01 22:44:16 +0200

    Readability for MowerTask Class

--- imow/common/mowertask.py
+++ imow/common/mowertask.py
@@ -2,9 +2,16 @@ from enum import Enum
 
 
 class MowerTask(Enum):
+    # Common
     STANDBY = 0
-    DOCK_ERROR = 1
+
+    # Mowing States
     EDGE_MOWING = 5
+
+    # Dock Relations
     INSIDE_DOCK_NO_CHARGING = 6
     INSIDE_DOCK_CHARGING = 7
     DRIVING_TO_DOCK = 11
+
+    # Errors
+    DOCK_ERROR = 1
